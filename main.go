50ade4698dee83f55e2293fffd5ab02700b615c1
towski <towski@gmail.com>
2014-09-26 01:27:02 -0700

    initialize the 3 servers separately, along with overall init

--- main.go
+++ main.go
@@ -2,23 +2,31 @@ package main
 
 import "net/http"
 import "log"
-import server "github.com/towski/artery/server"
+import artery "github.com/towski/artery/server"
 import dwarfomatic "github.com/towski/dwarfomatic/classes"
 
-func Init()  {
-    server.Dbmap_global.AddTableWithName(dwarfomatic.Dwarf{}, "Dwarf").SetKeys(false, "Id")
-    data_server := server.NewDataServer()
+func DataInit()  {
+    artery.Dbmap_global.AddTableWithName(dwarfomatic.Dwarf{}, "Dwarf").SetKeys(false, "Id")
+    data_server := artery.NewDataServer()
     data_server.Register(&dwarfomatic.DwarfServer{})
-    build_server := server.NewBuildServer()
-    build_server.Register(&dwarfomatic.ABuildServer{})
     go data_server.Start()
+}
+
+func BuildInit(){
+    build_server := artery.NewBuildServer()
+    build_server.Register(&dwarfomatic.ABuildServer{})
     go build_server.Start()
 }
 
-func main (){
+func HttpInit(){
     http.Handle("/df/artery/bar", &dwarfomatic.BarHandler{})
     http.Handle("/df/artery/submit_reaction", &dwarfomatic.SubmitReactionHandler{})
-    server.Init()
-    Init()
     log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main (){
+    artery.Init()
+    DataInit()
+    BuildInit()
+    HttpInit()
 }
